a9f9cb1d5ef4c08e745fe35fb2be3bf1f02e9e16
Liam Bray <127702658+1unarzDev@users.noreply.github.com>
2025-08-10 14:07:47 +0000

    update wal

--- files/.config/scripts/wal.sh
+++ files/.config/scripts/wal.sh
@@ -7,6 +7,7 @@ PYWAL_SPICETIFY="$HOME/.cache/wal/colors-spicetify.ini"
 SPICETIFY_CONFIG="$HOME/.config/spicetify/Themes/text/color.ini"
 HYPRLOCK_CONFIG="$HOME/.config/hypr/hyprlock.conf"
 MAKO_CONFIG="$HOME/.config/mako/config"
+SWAYOSD_DIR="$HOME/.config/swayosd"
 
 # Get all images
 images=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | sort)
@@ -30,7 +31,7 @@ x=$(echo "$x * $scale" | bc | cut -d'.' -f1)
 y=$(echo "$y * $scale" | bc | cut -d'.' -f1)
 
 cursor="$x,$y"
-swww img $image --transition-type grow --transition-pos $cursor --invert-y --transition-step 2 --transition-fps 144 --transition-duration 2
+swww img $image --transition-type grow --transition-pos $cursor --invert-y --transition-step 2 --transition-fps 60 --transition-duration 2
 sleep 1.1
 
 # Update pywal
@@ -39,23 +40,30 @@ wal -e -n -i "$image"
 # Terminal colors
 source ~/.cache/wal/colors.sh
 
-# Update config file with sed
+# Update config file
 sed -i "s/col\.active_border = .*/col.active_border = rgba(${color5#\#}ee) rgba(${color7#\#}ee) 45deg/" "$CONFIG_FILE"
 sed -i "s/col\.inactive_border = .*/col.inactive_border = rgba(${color8#\#}aa)/" "$CONFIG_FILE"
 
-# Cava colors
-sed -i "s/gradient_color_1 = .*/gradient_color_1 = '${color3}'/" ~/.config/cava/config
-sed -i "s/gradient_color_2 = .*/gradient_color_2 = '${color1}'/" ~/.config/cava/config
-pkill -USR1 cava
-
 # Hyprlock wallpaper
-sed -i "s|path = .*/\.config/swww/wallpapers/.*|path = $image|" "$HYPRLOCK_CONFIG"
+sed -i "s|path = .*|path = $image|" "$HYPRLOCK_CONFIG"
 
 # Mako notifications
 sed -i -e "s/background-color=#[0-9a-fA-F]\{6,8\}/background-color=${color0}dd/" \
        -e "s/text-color=#[0-9a-fA-F]\{6,8\}/text-color=${color15}dd/" \
        -e "s/border-color=#[0-9a-fA-F]\{6,8\}/border-color=${color4}dd/" $MAKO_CONFIG
 makoctl reload
+
+# Swayosd
+sed "s/BACKGROUND_COLOR/$background/g; \
+     s/BORDER_COLOR/$foreground/g; \
+     s/FOREGROUND_COLOR/$foreground/g; \
+     s/PROGRESS_BG_COLOR/$background/g; \
+     s/PROGRESS_COLOR/$foreground/g; \
+     s/ICON_COLOR/$foreground/g" \
+     $SWAYOSD_DIR/style.css.template > $SWAYOSD_DIR/style.css
+pkill -9 swayosd-server 2>/dev/null
+swayosd-server -s "$SWAYOSD_DIR/style.css" >/dev/null 2>&1 &
+disown
 
 # Also update runtime (for immediate effect)
 hyprctl keyword general:col.active_border "rgba(${color5#\#}ee) rgba(${color7#\#}ee) 45deg"
